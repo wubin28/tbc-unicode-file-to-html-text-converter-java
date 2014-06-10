99bbebb6239bbc366aff52d16e737818d3005999
Ben Wu <wubin28@gmail.com>
2014-06-10 22:46:26 +0800

    Added the original exercise code class UnicodeFileToHtmlTextConverter with a trivial test. Deleted class App and AppTest generated by Maven.

---
+++ ufthtc/src/main/java/kata/ufthtc/UnicodeFileToHtmlTextConverter.java
@@ -0,0 +1,35 @@
+package kata.ufthtc;
+
+import org.apache.commons.lang.StringEscapeUtils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Created by benwu on 14-6-10.
+ */
+public class UnicodeFileToHtmlTextConverter {
+    private String fullFilenameWithPath;
+
+    public UnicodeFileToHtmlTextConverter(String fullFilenameWithPath)
+    {
+        this.fullFilenameWithPath = fullFilenameWithPath;
+    }
+
+    public String convertToHtml() throws IOException{
+
+        BufferedReader reader = new BufferedReader(new FileReader(fullFilenameWithPath));
+
+        String line = reader.readLine();
+        String html = "";
+        while (line != null)
+        {
+            html += StringEscapeUtils.escapeHtml(line);
+            html += "<br />";
+            line = reader.readLine();
+        }
+        return html;
+
+    }
+}
---
+++ ufthtc/src/test/java/kata/ufthtc/UnicodeFileToHtmlTextConverterTest.java
@@ -0,0 +1,15 @@
+package kata.ufthtc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by benwu on 14-6-10.
+ */
+public class UnicodeFileToHtmlTextConverterTest {
+    @Test
+    public void a_trivial_test() {
+        assertEquals(5, 2 + 3);
+    }
+}
