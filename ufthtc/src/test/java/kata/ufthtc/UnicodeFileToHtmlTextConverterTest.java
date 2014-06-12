b6d0c5d421d91721ecf7ffb65d56a39f52dd4507
Ben Wu <wubin28@gmail.com>
2014-06-12 23:34:32 +0800

    Added the intention code for the Arrange part of test should_convert_ampersand().

--- ufthtc/src/test/java/kata/ufthtc/UnicodeFileToHtmlTextConverterTest.java
+++ ufthtc/src/test/java/kata/ufthtc/UnicodeFileToHtmlTextConverterTest.java
@@ -8,14 +8,15 @@ import static org.junit.Assert.assertEquals;
  * Created by benwu on 14-6-10.
  */
 public class UnicodeFileToHtmlTextConverterTest {
-    @Test
-    public void a_trivial_test() {
-        assertEquals(5, 2 + 3);
-    }
-
     // TODO-acceptance-test-working-on: should convert ampersand
     @Test
     public void should_convert_ampersand() {
+        // Arrange
+        StringSource stringSource = new StringSource();
+        stringSource.setSourceString("H&M");
+        UnicodeFileToHtmlTextConverter converter = new UnicodeFileToHtmlTextConverter(stringSource);
+
+        // Act
         // Assert
         assertEquals("H&amp;M<br />", converter.convertToHtml());
     }
