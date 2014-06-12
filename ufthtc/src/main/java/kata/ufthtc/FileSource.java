8b61547baa729bcc70183c77573242e074db5037
Ben Wu <wubin28@gmail.com>
2014-06-13 00:15:22 +0800

    Implemented method FileSource.getReader() to create a FileReader instance.

--- ufthtc/src/main/java/kata/ufthtc/FileSource.java
+++ ufthtc/src/main/java/kata/ufthtc/FileSource.java
@@ -1,17 +1,27 @@
 package kata.ufthtc;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.Reader;
 
 /**
  * Created by benwu on 14-6-13.
  */
 public class FileSource implements ConvertingSource {
+    private String fullFilenameWithPath;
+
     @Override
     public Reader getReader() {
-        return null;
+        FileReader fileReader = null;
+        try {
+            fileReader = new FileReader(this.fullFilenameWithPath);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return fileReader;
     }
 
     public void setFullFilenameWithPath(String fullFilenameWithPath) {
-        
+        this.fullFilenameWithPath = fullFilenameWithPath;
     }
 }
