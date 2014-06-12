d6e67bac7339587da6e05e0de8950df4c8da4702
Ben Wu <wubin28@gmail.com>
2014-06-12 23:41:44 +0800

    Created method StringSource.setSourceString().

--- ufthtc/src/main/java/kata/ufthtc/StringSource.java
+++ ufthtc/src/main/java/kata/ufthtc/StringSource.java
@@ -4,4 +4,7 @@ package kata.ufthtc;
  * Created by benwu on 14-6-12.
  */
 public class StringSource implements ConvertingSource {
+    public void setSourceString(String sourceString) {
+
+    }
 }
