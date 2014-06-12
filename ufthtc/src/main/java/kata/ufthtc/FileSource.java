d926e07490e63b261d14ff074d196ed0a4867f83
Ben Wu <wubin28@gmail.com>
2014-06-13 00:05:55 +0800

    Created class FileSource and made it implement the interface ConvertingSource. Implemented method getReader() in it.

---
+++ ufthtc/src/main/java/kata/ufthtc/FileSource.java
@@ -0,0 +1,13 @@
+package kata.ufthtc;
+
+import java.io.Reader;
+
+/**
+ * Created by benwu on 14-6-13.
+ */
+public class FileSource implements ConvertingSource {
+    @Override
+    public Reader getReader() {
+        return null;
+    }
+}
