b0c59b2aeebcb9575c9d04714c53b22efc174e7a
Ben Wu <wubin28@gmail.com>
2014-06-13 09:21:50 +0800

    Implemented method escape() in class ApacheCommonsEscaper.

--- ufthtc/src/main/java/kata/ufthtc/ApacheCommonsEscaper.java
+++ ufthtc/src/main/java/kata/ufthtc/ApacheCommonsEscaper.java
@@ -1,7 +1,13 @@
 package kata.ufthtc;
 
+import org.apache.commons.lang.StringEscapeUtils;
+
 /**
  * Created by benwu on 14-6-13.
  */
 public class ApacheCommonsEscaper implements HtmlEscaper {
+    @Override
+    public String escape(String string) {
+        return StringEscapeUtils.escapeHtml(string);
+    }
 }
