669c665bc26bc8493148bd7cb5218900b1fe0c73
Ben Wu <wubin28@gmail.com>
2014-06-13 08:37:46 +0800

    Removed duplicate code in the test class.

--- ufthtc/src/test/java/kata/ufthtc/UnicodeFileToHtmlTextConverterTest.java
+++ ufthtc/src/test/java/kata/ufthtc/UnicodeFileToHtmlTextConverterTest.java
@@ -1,5 +1,6 @@
 package kata.ufthtc;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -10,12 +11,20 @@ import static org.junit.Assert.assertEquals;
  * Created by benwu on 14-6-10.
  */
 public class UnicodeFileToHtmlTextConverterTest {
+    private StringSource stringSource;
+    private UnicodeFileToHtmlTextConverter converter;
+
+    @Before
+    public void initialize() {
+        // Arrange
+        stringSource = new StringSource();
+        converter = new UnicodeFileToHtmlTextConverter(stringSource);
+    }
+
     @Test
     public void should_convert_ampersand() throws IOException{
         // Arrange
-        StringSource stringSource = new StringSource();
         stringSource.setSourceString("H&M");
-        UnicodeFileToHtmlTextConverter converter = new UnicodeFileToHtmlTextConverter(stringSource);
 
         // Act
         // Assert
@@ -25,9 +34,7 @@ public class UnicodeFileToHtmlTextConverterTest {
     @Test
     public void should_convert_greater_than_and_less_than() throws IOException{
         // Arrange
-        StringSource stringSource = new StringSource();
         stringSource.setSourceString(">_<|||");
-        UnicodeFileToHtmlTextConverter converter = new UnicodeFileToHtmlTextConverter(stringSource);
 
         // Act
         // Assert
