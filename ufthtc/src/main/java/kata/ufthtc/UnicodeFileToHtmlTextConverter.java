08d8848060f72395943bc5eff020e9a78aa313d2
Ben Wu <wubin28@gmail.com>
2014-08-02 19:00:01 +0800

    Fixing null pointer exception: Wrote intention code in method UnicodeFileToHtmlTextConverter.convertToHtml() to depend on the interface Reader instead of a concrete FileReader object.

--- ufthtc/src/main/java/kata/ufthtc/UnicodeFileToHtmlTextConverter.java
+++ ufthtc/src/main/java/kata/ufthtc/UnicodeFileToHtmlTextConverter.java
@@ -22,7 +22,7 @@ public class UnicodeFileToHtmlTextConverter {
     public String convertToHtml() throws IOException{
 
         // TODO-working-on: Depending on the file system violates the Dependency Inversion Principle and Open-Closed Principle
-        BufferedReader reader = new BufferedReader(new FileReader(fullFilenameWithPath));
+        BufferedReader reader = new BufferedReader(this.reader);
 
         String line = reader.readLine();
         String html = "";
