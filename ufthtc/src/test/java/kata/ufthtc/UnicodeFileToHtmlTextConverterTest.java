7018a7d3a172284f2910eb6218a4f1e4701b40d4
Ben Wu <wubin28@gmail.com>
2014-08-02 18:26:13 +0800

    Working on TODO: should convert ampersand. Wrote test should_convert_ampersand() for it with the Assert part.

--- ufthtc/src/test/java/kata/ufthtc/UnicodeFileToHtmlTextConverterTest.java
+++ ufthtc/src/test/java/kata/ufthtc/UnicodeFileToHtmlTextConverterTest.java
@@ -8,14 +8,15 @@ import static org.junit.Assert.assertEquals;
  * Created by benwu on 14-6-10.
  */
 public class UnicodeFileToHtmlTextConverterTest {
+
+    // TODO-acceptance-test-working-on: should convert ampersand
     @Test
-    public void a_trivial_test() {
-        assertEquals(5, 2 + 3);
+    public void should_convert_ampersand() {
+        // Assert
+        assertEquals("H&amp;M<br />", converter.convertToHtml());
     }
 
-    // TODO-new-feature: Make the method convertToHtml() working for not only a file but also a string
-
-    // TODO-acceptance-test: should convert ampersand
     // TODO-acceptance-test: should convert greater than and less than
     // TODO-acceptance-test: should add a line break for a new line
+    // TODO-new-feature: Make the method convertToHtml() working for not only a file but also a string
 }
