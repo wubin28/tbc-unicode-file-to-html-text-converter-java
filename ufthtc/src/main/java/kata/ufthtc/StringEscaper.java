b9dc741f515cafef90631f066d6c7521e2e6dadb
Ben Wu <wubin28@gmail.com>
2014-08-02 21:03:26 +0800

    Fixed comparision failure: Implemented method StringEscaper.escapeHtml().

--- ufthtc/src/main/java/kata/ufthtc/StringEscaper.java
+++ ufthtc/src/main/java/kata/ufthtc/StringEscaper.java
@@ -1,10 +1,12 @@
 package kata.ufthtc;
 
+import org.apache.commons.lang.StringEscapeUtils;
+
 /**
  * Created by benwu on 14-8-2.
  */
 public class StringEscaper {
     public String escapeHtml(String originalString) {
-        return null;
+        return StringEscapeUtils.escapeHtml(originalString);
     }
 }
